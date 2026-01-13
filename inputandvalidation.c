f6db05f407d0f4c40057048ffdf004d8feff342a
Adinikh <adityas5584761@gmail.com>
2026-01-13 16:01:58 +0530

    dd

---
+++ inputandvalidation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Student{
+    char id[10],name[20],grade;
+     int m[5],total;
+     float per;
+};
+
+char getGrade(float p){
+    if(p>=90) return 'O';
+    else if(p>=85);
+
+}
+int main(){
+
+}
